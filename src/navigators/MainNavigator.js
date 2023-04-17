fa9e358573707dabca2329e109beb20f3c2573ea
unknown <nicolasarcieri@gmail.com>
2023-04-17 15:39:28 -0300

    OctavaEntregaV0.85

--- src/navigators/MainNavigator.js
+++ src/navigators/MainNavigator.js
@@ -5,8 +5,10 @@ import AuthNavigator from "./AuthNavigator";
 import { useSelector } from "react-redux";
 
 const MainNavigator = () => {
-  //const isAuth = useSelector((state) => state.auth.userId);
-  const isAuth=true
+  const isAuth = useSelector((state) => state.auth.userId);
+  //La siguiente linea la uso cuando tengo que probar cosas y ahorrar tiempo
+  //con la autentificacion que sé que funciona
+  //const isAuth=true
   return (
     <NavigationContainer>
       {isAuth ? <TabsNavigator /> : <AuthNavigator />}
